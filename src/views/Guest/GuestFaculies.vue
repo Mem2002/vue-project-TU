91de0bbb105bf82254109e79be030d91ea822334
Mem2002 <vannamanh5@gmail.com>
2024-04-11 08:49:21 +0700

    update

--- src/views/Guest/GuestFaculies.vue
+++ src/views/Guest/GuestFaculies.vue
@@ -1,30 +1,34 @@
 <template>
-    <div class="card" style="width: 78rem">
-      <h5 class="card-title" style="padding: 10px">Welcome Guest!</h5>
-      <div class="card-body" style="margin: 110px">
-        <div class="container">
-          <div class="row">
-            <div class="col-12 col-md-6 col-lg-4">
-              <div class="card">
-                <img src="../../assets/imagebrain.jpg" alt="test" class="card-img-top" />
-                <div class="card-body">
-                  <h5 class="card-title">IT</h5>
-                  <p class="card-text custom-right-align ml-10">
-                    <button type="button" class="btn btn-info">Dowload</button>
-                  </p>
-                </div>
+  <div class="card" style="width: 78rem">
+    <h5 class="card-title" style="padding: 10px">Welcome Guest!</h5>
+    <div class="card-body" style="margin: 110px">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+              <img
+                src="../../assets/imagebrain.jpg"
+                alt="test"
+                class="card-img-top"
+              />
+              <div class="card-body">
+                <h5 class="card-title">IT</h5>
+                <p class="card-text custom-right-align ml-10">
+                  <button type="button" class="btn btn-info">Dowload</button>
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
             
             <script>
-  import router from "../../router/index";
-  import axios from "axios";
-  export default {
+import router from "../../router/index";
+import axios from "axios";
+export default {
   data() {
     return {
       results: {},
@@ -45,20 +49,20 @@
   methods: {
     getlistrole() {
       axios
-        .get("http://localhost:8081/v1/contribution/readforGuest",{
+        .get("http://localhost:8081/v1/contribution/readforGuest", {
           withCredentials: true,
         })
         .then((data) => {
-          console.log(data,);
+          console.log(data);
           // this.listtopic = data.data.DT;
         });
     },
   },
 };
-  </script>
+</script>
             
   <style>
-  .custom-right-align {
-    text-align: center;
-  }
-  </style>
+.custom-right-align {
+  text-align: center;
+}
+</style>
