2275180f9c609b6921d987ce3cbc3f7d0f8089e0
Mem2002 <vannamanh5@gmail.com>
2024-04-17 15:23:11 +0700

    update

--- src/views/Admin/Chart.vue
+++ src/views/Admin/Chart.vue
@@ -89,7 +89,7 @@
               aria-valuenow="50"
               aria-valuemin="0"
               aria-valuemax="100"
-              style="width: 72%; background-color: #51258a"
+              style="width: 72%; "
             ></div>
           </div>
           <br />
