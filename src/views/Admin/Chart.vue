89e573372ef2a3a89dc17f88399d3b9792dba0a9
Mem2002 <vannamanh5@gmail.com>
2024-04-17 16:22:36 +0700

    update

--- src/views/Admin/Chart.vue
+++ src/views/Admin/Chart.vue
@@ -81,12 +81,12 @@
           </div> -->
           <br />
           <div
-            class="h-50 mx-3 bg-secondary bg-opacity-50 rounded"
+            class="h-50px mx-3 bg-secondary bg-opacity-50 rounded"
             style="height: 10%"
             v-if="listdashboard.length > 0"
           >
             <div
-              class="rounded w-50"
+              class="rounded w-50px"
               role="progressbar"
               aria-valuenow="50"
               aria-valuemin="0"
