0583dde6dc784d8e469e5d8811011df3b967abae
Mem2002 <vannamanh5@gmail.com>
2024-04-17 15:13:08 +0700

    update

--- src/views/Admin/Chart.vue
+++ src/views/Admin/Chart.vue
@@ -146,7 +146,8 @@ export default {
       axios
         .get(
           "https://backend-final-zk84.onrender.com/v1/dashboardAdmin",
-          this.post
+          this.post,
+          console.log(data.data.DT[0].faculty.dashbroadFaculty.percent)
         )
         .then((data) => {
           console.log(data);
