c91d20d748689ad1efa17195688b94aa4b53eb90
Khoatruong17 <109325940+Khoatruong17@users.noreply.github.com>
2024-04-19 09:40:27 +0700

    ok

--- src/views/Students/Submit.vue
+++ src/views/Students/Submit.vue
@@ -4,59 +4,21 @@
       <h5 class="card-title">Submit My Post</h5>
       <from method="post" @submit="uploadData">
         <div class="mb-3 bg p-5 rounded">
-          <label
-            for="exampleFormControlInput1"
-            class="form-label mt-4 fw-semibold"
-            >Name</label
-          >
-          <input
-            type="name"
-            class="form-control"
-            id="exampleFormControlInput1"
-            v-model="upload.name"
-          />
-          <label
-            for="exampleFormControlInput1"
-            class="form-label mt-4 fw-semibold"
-            >Description</label
-          >
-          <input
-            type="description"
-            class="form-control"
-            id="exampleFormControlInput1"
-            v-model="upload.description"
-          />
-          <label
-            for="exampleFormControlInput1"
-            class="form-label mt-4 fw-semibold"
-            >File</label
-          >
+          <label for="exampleFormControlInput1" class="form-label mt-4 fw-semibold">Name</label>
+          <input type="name" class="form-control" id="exampleFormControlInput1" v-model="upload.name" />
+          <label for="exampleFormControlInput1" class="form-label mt-4 fw-semibold">Description</label>
+          <input type="description" class="form-control" id="exampleFormControlInput1" v-model="upload.description" />
+          <label for="exampleFormControlInput1" class="form-label mt-4 fw-semibold">File</label>
           <div class="input-group mb-3 mt-3">
-            <input
-              type="file"
-              class="form-control"
-              id="inputGroupFile01"
-              name="file"
-              @change="addFile"
-              enctype="multipart/form-data"
-            />
+            <input type="file" class="form-control" id="inputGroupFile01" name="file" @change="addFile"
+              enctype="multipart/form-data" />
           </div>
-          <div
-            class="modal fade"
-            id="exampleModalToggle"
-            aria-hidden="true"
-            aria-labelledby="exampleModalToggleLabel"
-            tabindex="-1"
-          >
+          <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
+            tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
               <div class="modal-content">
                 <div class="modal-header">
-                  <button
-                    type="button"
-                    class="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                  ></button>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                   I commit to comply with and fully accept the terms and
@@ -64,40 +26,21 @@
                   contents before continuing to use.
                 </div>
                 <div class="modal-footer">
-                  <button
-                    class="btn btn-primary"
-                    data-bs-target="#exampleModalToggle2"
-                    data-bs-toggle="modal"
-                    @click="agree"
-                  > 
-                  Agree
+                  <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"
+                    @click="agree">
+                    Agree
                   </button>
                 </div>
               </div>
             </div>
           </div>
           <div class="ml-3 mt-10">
-            <input
-              class="form-check-input mt-1"
-              type="checkbox"
-              value=""
-              aria-label="Checkbox for following text input"
-              v-model="agreeTerms"
-            />
-            <a
-              href=""
-              class="text-decoration-none"
-              data-bs-target="#exampleModalToggle"
-              data-bs-toggle="modal"
-              >I agree with Terms & Conditions</a
-            >
+            <input class="form-check-input mt-1" type="checkbox" value="" aria-label="Checkbox for following text input"
+              v-model="agreeTerms" />
+            <a href="" class="text-decoration-none" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">I agree
+              with Terms & Conditions</a>
           </div>
-          <button
-            type="submit"
-            class="btn btn-primary mt-5"
-            @click="uploadData()"
-            :disabled="!agreeTerms"
-          >
+          <button type="submit" class="btn btn-primary mt-5" @click="uploadData()" :disabled="!agreeTerms">
             Upload
           </button>
         </div>
@@ -105,8 +48,8 @@
     </div>
   </div>
 </template>
-        
-<script >
+
+<script>
 import axios from "axios";
 import Cookies from 'js-cookie';
 export default {
@@ -140,14 +83,14 @@ export default {
       formData.append("description", this.upload.description);
       formData.append("file", this.upload.file);
       formData.append("topic_id", this.userId);
-      alert("Uploaded successfully");
-      console.log(this.upload.name);
       axios
         .post("https://backend-final-zk84.onrender.com/v1/contribution/create", formData, {
           withCredentials: true,
-        }) 
+        })
         .then((data) => {
           console.log(data);
+          alert("Uploaded successfully");
+          console.log(this.upload.name);
         })
         .catch((error) => {
           console.error("Error:", error);
@@ -161,6 +104,5 @@ export default {
   },
 };
 </script>
-        
-        <style>
-</style>
+
+<style></style>
